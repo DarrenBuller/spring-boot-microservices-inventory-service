f54a73b290285a5630efc0468018324b44c465ea
Darren Buller <stash-taco-remake@duck.com>
2024-10-09 10:39:57 +0100

    Add initial Kafka and switch to host networking

--- src/test/java/com/example/inventory/InventoryServiceApplicationTests.java
+++ src/test/java/com/example/inventory/InventoryServiceApplicationTests.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
+	@SuppressWarnings("rawtypes")
 	@ServiceConnection
 	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
 	@LocalServerPort
